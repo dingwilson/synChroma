a901d6774a4e980476f35c5a0958b5c8320aac3d
Wilson Ding <shodai100@gmail.com>
2016-10-01 17:58:17 -0500

    Added GPUImage

--- synChroma/ChromaMessengerViewController.swift
+++ synChroma/ChromaMessengerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChromaMessengerViewController.swift
 //  synChroma
 //
 //  Created by Wilson Ding on 10/1/16.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ChromaMessengerViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
