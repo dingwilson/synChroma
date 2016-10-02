14d13a0b683f3036d80cc5135b05b95ce53a2afb
Wilson Ding <shodai100@gmail.com>
2016-10-02 01:59:27 -0500

    Added color reading module

--- synChroma/ChromaMessengerViewController.swift
+++ synChroma/ChromaMessengerViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import GPUImage
 
 class ChromaMessengerViewController: UIViewController {
+    
+    var videoCamera: GPUImageVideoCamera?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupCamera()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func setupCamera() {
+        videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset1280x720, cameraPosition: .back)
+        videoCamera!.outputImageOrientation = .portrait
+        
+        let cropFilter = GPUImageCropFilter()
+        cropFilter.cropRegion = CGRect(x: 0.35, y: 0.35, width: 0.3, height: 0.3)
+        
+        let medianFilter = GPUImageMedianFilter()
+        
+        cropFilter.addTarget(medianFilter)
+        
+        let averageColorFilter = GPUImageAverageColor()
+        
+        medianFilter.addTarget(averageColorFilter)
+        
+        averageColorFilter.colorAverageProcessingFinishedBlock = {(redComponent, greenComponent, blueComponent, alphaComponent, frameTime) in
+
+            print("Average color: \(redComponent*255), \(greenComponent*255), \(blueComponent*255), \(alphaComponent)")
+        }
+        
+        videoCamera?.addTarget(averageColorFilter)
+        averageColorFilter.addTarget(self.view as! GPUImageView)
+        videoCamera?.startCapture()
     }
+
 }
