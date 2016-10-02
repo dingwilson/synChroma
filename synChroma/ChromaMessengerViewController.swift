ce1cde38744e77a829740ee8c9696c86abc4f091
Wilson Ding <shodai100@gmail.com>
2016-10-02 08:37:37 -0500

    Modified to increase fps

--- synChroma/ChromaMessengerViewController.swift
+++ synChroma/ChromaMessengerViewController.swift
@@ -41,26 +41,19 @@ class ChromaMessengerViewController: UIViewController {
         let cropFilter = GPUImageCropFilter()
         cropFilter.cropRegion = CGRect(x: 0.35, y: 0.35, width: 0.3, height: 0.3)
         
-        let medianFilter = GPUImageMedianFilter()
-        
-        cropFilter.addTarget(medianFilter)
-        
         let averageColorFilter = GPUImageAverageColor()
         
-        medianFilter.addTarget(averageColorFilter)
+        cropFilter.addTarget(averageColorFilter)
         
         averageColorFilter.colorAverageProcessingFinishedBlock = {(redComponent, greenComponent, blueComponent, alphaComponent, frameTime) in
 
             print("Average color: \(redComponent*255), \(greenComponent*255), \(blueComponent*255), \(alphaComponent)")
             
-            
             self.convertRGB(red: Float(redComponent*255), green: Float(greenComponent*255), blue: Float(blueComponent*255))
             
             print(self.myCurrentTranscribed + "\n")
             
-            let array = self.myCurrentTranscribed.components(separatedBy: "545")
-            
-            if array.count == 3 {
+            if self.myCurrentTranscribed.components(separatedBy: "545").count == 3 {
                 print(self.convertBaseThreeToText(str: self.myCurrentTranscribed))
                 self.myCurrentTranscribed = ""
             }
@@ -107,8 +100,7 @@ class ChromaMessengerViewController: UIViewController {
     }
     
     func convertRGB(red: Float, green: Float, blue: Float) {
-        let tempUpdated = isNew(red: red, green: green, blue: blue)
-        if (tempUpdated) {
+        if isNew(red: red, green: green, blue: blue) {
             if (blue < 125.0 && green < 125.0 && red < 125.0) {
                 myCurrentTranscribed = myCurrentTranscribed + "5"
                 lastUniqueValue = "black"
@@ -130,15 +122,14 @@ class ChromaMessengerViewController: UIViewController {
     
     func convertBaseThreeToText(str: String) -> String {
         let myArray = str.components(separatedBy: "5");
-        print(myArray);
         var endString : String = "";
         for i in (0...myArray.count-1) {
             let number = Int(strtoul(myArray[i], nil, 3))
             let u = UnicodeScalar(number)
             endString = endString + String(u!);
         }
-        
-        return (endString)
+        print(endString)
+        return endString
     }
 
 }
